4661acfc40531e34a63dceb63ea423899846f2d6
Kosta Korenkov <kosta@whoot.me>
2020-03-31 19:39:31 +0300

    fix: don't keep dist under git control

--- packages/client/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare class WhiteRabbitClient {
-    private iframe;
-    url(tokenId: any): string;
-    ensureIFrame(url: any): Promise<any>;
-    requestPayment(tokenId: string): Promise<void>;
-}
-declare const _default: WhiteRabbitClient;
-export default _default;
