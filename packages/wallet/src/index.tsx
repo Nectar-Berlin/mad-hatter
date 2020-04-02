3e6da23e3292c1d5e77e55a708b3899683714d3e
Kosta Korenkov <kosta@whoot.me>
2020-04-02 14:11:01 +0300

    chore(deps): remove react-router-dom, burner exchange and unused typedefs

--- packages/wallet/src/index.tsx
+++ packages/wallet/src/index.tsx
@@ -4,7 +4,6 @@ import { xdai } from '@burner-wallet/assets';
 import BurnerCore from '@burner-wallet/core';
 import { LocalSigner } from '@burner-wallet/core/signers';
 import { XDaiGateway, } from '@burner-wallet/core/gateways';
-import Exchange, { XDaiBridge } from '@burner-wallet/exchange';
 import ModernUI from '@burner-wallet/modern-ui';
 
 const core = new BurnerCore({
@@ -13,10 +12,6 @@ const core = new BurnerCore({
     new XDaiGateway(),
   ],
   assets: [xdai],
-});
-
-const exchange = new Exchange({
-  pairs: [new XDaiBridge()],
 });
 
 const BurnerWallet = () =>
