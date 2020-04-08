68d625c7d375b8ed7eb21a4bdfac44c9c82a6537
Kosta Korenkov <kosta@whoot.me>
2020-04-08 17:55:13 +0300

    drop console.log

--- packages/streaming-site-example/app.tsx
+++ packages/streaming-site-example/app.tsx
@@ -7,7 +7,6 @@ const client = new WhiteRabbitClient(process.env.WR_WALLET_HOST);
 const StreamingApp = () => {
 
   const openWhiteRabbit = () => {
-    console.log('Who is there?');
     client.requestPayment('2142160385');
   };
 
